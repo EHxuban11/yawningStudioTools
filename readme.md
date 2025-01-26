a63b132efe9d98dac26f9379b950c60958c24d81
Xuban <59646791+EHxuban11@users.noreply.github.com>
2025-01-27 00:15:02 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+
+
+
+# Pure frontend tools to inspire from
+Audio trimmer: https://audiotrimmer.com/
+Fade-in Instagram reel: https://yawningstudio.com/
---
+++ yawning-studio-tools-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Yawning Studio Tools</h1>
+    </div>
+  );
+}
+
+export default App;
